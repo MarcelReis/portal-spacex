96ade5babdebf8cbbf7c40f19912d527348d2050
Marcelo Reis <oi@marcelreis.dev>
2021-05-22 23:04:50 -0300

    chore: adds materialize css

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import "materialize-css/dist/css/materialize.min.css";
+import "material-design-icons/iconfont/material-icons.css";
+
 import { createApp } from "vue";
 import App from "./App.vue";
 import "@/registerServiceWorker";
