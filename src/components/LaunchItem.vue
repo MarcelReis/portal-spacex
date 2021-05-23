bcc42a2f228b6164353388d168b2b83a5fd1c120
Marcelo Reis <oi@marcelreis.dev>
2021-05-23 10:24:45 -0300

    feat: style launches list

--- src/components/LaunchItem.vue
+++ src/components/LaunchItem.vue
@@ -1,29 +1,62 @@
 <template>
-  <li>
-    <div>
-      <img :src="thumb ?? 'https://via.placeholder.com/50'" alt="" />
-
-      <div>
-        <h2>{{ name }}</h2>
-        <br />
-        <span>{{ date }}</span>
+  <li class="p-3 border-gray-100 border-2 rounded-md">
+    <header class="flex gap-4 items-center">
+      <div class="w-10 h-10 rounded-full">
+        <img
+          :src="thumb ?? 'https://via.placeholder.com/50'"
+          alt=""
+          class="object-cover object-center w-full h-full"
+        />
       </div>
+
+      <div class="flex flex-col">
+        <h2 class="font-bold text-lg">{{ name }}</h2>
+        <span class="text-sm text-gray-700">{{ date }}</span>
+      </div>
+
+      <div class="ml-auto">
+        <span
+          class="rounded-sm px-2 py-0.5 text-white bg-green-700"
+          v-if="success && !upcoming"
+        >
+          Success
+        </span>
+        <span
+          class="rounded-sm px-2 py-0.5 text-white bg-red-700"
+          v-else-if="!success && !upcoming"
+          >Fail</span
+        >
+      </div>
+    </header>
+
+    <div class="my-3">
+      <p class="description">{{ details }}</p>
     </div>
 
-    <span>
-      <span v-if="success && !upcoming"> Success </span>
-      <span v-else-if="!success && !upcoming">Fail</span>
-    </span>
+    <footer class="flex justify-end gap-2">
+      <span
+        class="text-xs rounded-sm px-1 py-0.5 text-white bg-gray-500"
+        v-if="payloads.length"
+      >
+        {{ payloads.length }} Payload(s)
+      </span>
+      <span
+        class="text-xs rounded-sm px-1 py-0.5 text-white bg-gray-500"
+        v-else
+      >
+        No Payload
+      </span>
 
-    <p>{{ details }}</p>
-
-    <div>
-      <span v-if="payloads.length"> {{ payloads.length }} Payload(s) </span>
-      <span v-else>No Payload</span>
-
-      <span v-if="crew.length"> {{ crew.length }} Astronaut(s) </span>
-      <span v-else>No Crew</span>
-    </div>
+      <span
+        class="text-xs rounded-sm px-1 py-0.5 text-white bg-gray-500"
+        v-if="crew.length"
+      >
+        {{ crew.length }} Astronaut(s)
+      </span>
+      <span class="text-xs rounded-sm px-1 py-0.5 text-white bg-gray-500" v-else
+        >No Crew</span
+      >
+    </footer>
   </li>
 </template>
 
@@ -46,4 +79,12 @@ export default defineComponent({
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.description {
+  height: 72px;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+}
+</style>
