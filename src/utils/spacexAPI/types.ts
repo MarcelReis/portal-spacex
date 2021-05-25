d607cfa1243e5bf1472924fe67d7de34cd9bcf64
Marcelo Reis <oi@marcelreis.dev>
2021-05-25 18:23:34 -0300

    types: update type definition

--- src/utils/spacexAPI/types.ts
+++ src/utils/spacexAPI/types.ts
@@ -1,13 +1,13 @@
 export interface Fairings {
-  reused?: boolean;
-  recovery_attempt?: boolean;
-  recovered?: boolean;
+  reused: boolean | null;
+  recovery_attempt: boolean | null;
+  recovered: boolean | null;
   ships: string[];
 }
 
 export interface Patch {
-  small: string;
-  large: string;
+  small: string | null;
+  large: string | null;
 }
 
 export interface Reddit {
@@ -27,10 +27,10 @@ export interface Links {
   reddit: Reddit;
   flickr: Flickr;
   presskit: string | null;
-  webcast: string;
-  youtube_id: string;
-  article: string;
-  wikipedia: string;
+  webcast: string | null;
+  youtube_id: string | null;
+  article: string | null;
+  wikipedia: string | null;
 }
 
 export interface Failure {
@@ -40,28 +40,28 @@ export interface Failure {
 }
 
 export interface Core {
-  core: string;
-  flight: number;
-  gridfins: boolean;
-  legs: boolean;
-  reused: boolean;
-  landing_attempt: boolean;
+  core: string | null;
+  flight: number | null;
+  gridfins: boolean | null;
+  legs: boolean | null;
+  reused: boolean | null;
+  landing_attempt: boolean | null;
   landing_success?: boolean | null;
   landing_type: string | null;
   landpad: string | null;
 }
 
 export interface LaunchesResponse {
-  fairings: Fairings;
+  fairings: Fairings | null;
   links: Links;
   static_fire_date_utc?: string | null;
-  static_fire_date_unix?: number;
+  static_fire_date_unix?: number | null;
   tbd: boolean;
   net: boolean;
-  window?: number;
+  window: number | null;
   rocket: string;
-  success: boolean;
-  details: string;
+  success: boolean | null;
+  details: string | null;
   crew: string[];
   ships: string[];
   capsules: string[];
